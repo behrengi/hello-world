e34c10fe25e9cff51c207f68daf76cf58eed185d
Bihter Akyol <bakyol17@ku.edu.tr>
2018-03-14 21:30:11 +0300

    Initial commit.

---
+++ utkan.jl
@@ -0,0 +1,13 @@
+function fib(x)
+    if x == 0
+        return 0
+    elseif x == 1
+        return 1
+    else 
+        return fib(x - 1) + fib(x - 2)
+    end
+end
+
+for i in 1:10
+    println(i, fib(i))
+end
